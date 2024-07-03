3fbbb70a303afa7434c89c63082dcd5410a35990
Quinten De Man <quinten.deman@gmail.com>
2024-07-03 15:51:59 -0400

    created a binary stream file benchmark class, we can now run on streams

---
+++ src/jmh/kotlin/benchmarks/StreamFileBenchmarks.kt
@@ -0,0 +1,47 @@
+package benchmarks
+
+import benchmarks.util.*
+import benchmarks.util.executors.SuccessiveScenarioExecutor
+import benchmarks.util.generators.StreamFileScenarioGenerator
+import org.openjdk.jmh.annotations.*
+import java.util.concurrent.TimeUnit
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Measurement(iterations = iterations, time = TIME_IN_SECONDS, timeUnit = TimeUnit.SECONDS)
+@Warmup(iterations = warmupIterations, time = TIME_IN_SECONDS, timeUnit = TimeUnit.SECONDS)
+open class StreamFileBenchmark {
+
+    lateinit var scenario: Scenario
+    lateinit var scenarioExecutor: SuccessiveScenarioExecutor
+
+    @Param
+    open var dcpConstructor: DCPForModificationsConstructor = DCPForModificationsConstructor.values()[0]
+
+    @Param("1", "2", "4", "8", "16", "32", "64", "128", "144")
+    open var workers: Int = 0
+
+    @Benchmark
+    fun benchmark() {
+        scenarioExecutor.run()
+    }
+
+    @Setup(Level.Trial)
+    fun initialize() {
+        scenario = StreamFileScenarioGenerator()
+            .generate("/mnt/ssd1/binary_streams/kron_13_stream_binary", workers)
+    }
+
+    @Setup(Level.Invocation)
+    fun initializeInvocation() {
+        scenarioExecutor = SuccessiveScenarioExecutor(
+            scenario,
+            { size -> dcpConstructor.constructor()(size, workers + 1) })
+    }
+
+    @Setup(Level.Iteration)
+    fun flushOut() {
+        println()
+    }
+}
--- src/main/kotlin/benchmarks/util/generators/StreamFileScenarioGenerator.kt
+++ src/main/kotlin/benchmarks/util/generators/StreamFileScenarioGenerator.kt
@@ -3,6 +3,7 @@ package benchmarks.util.generators
 import benchmarks.util.QueryType
 import benchmarks.util.Scenario
 import benchmarks.util.edgeToQuery
+import benchmarks.util.MAX_BITS_PER_NODE
 
 import java.io.File
 import java.math.BigInteger
@@ -14,15 +15,22 @@ class StreamFileScenarioGenerator {
 
         val num_nodes_bytes = ByteArray(4)
         stream.read(num_nodes_bytes)
+        num_nodes_bytes.reverse()
         val num_nodes = BigInteger(num_nodes_bytes).toInt()
+        print("Num nodes: ")
+        println(num_nodes)
 
         val num_updates_bytes = ByteArray(8)
         stream.read(num_updates_bytes)
+        num_updates_bytes.reverse()
         val num_updates_long = BigInteger(num_updates_bytes).toLong()
+        print("Num updates: ")
+        println(num_updates_long)
         val num_updates: Int
-        if (num_updates_long >= 100000000) {
-            num_updates = 100000000
-            println("UPPER BOUND OF BUFFER REACHED")
+        val max_updates = 100000000
+        if (num_updates_long >= max_updates) {
+            num_updates = max_updates
+            // println("UPPER BOUND OF BUFFER REACHED")
         } else {
             num_updates = num_updates_long.toInt()
         }
@@ -34,11 +42,21 @@ class StreamFileScenarioGenerator {
                     val type_buffer: Int
                     type_buffer = stream.read()
                     stream.read(buffer)
+                    buffer.reverse()
                     val edge = BigInteger(buffer).toLong()
+
+                    val src = edge.toInt().toLong()
+                    val dst = edge - src
+                    val shifted_dst = dst shr (32 - MAX_BITS_PER_NODE)
+                    val real_edge = shifted_dst + src
+
+                    // println(type_buffer)
+                    // println(edge.toInt())
+                    // println(edge shr 32)
                     if (type_buffer == 0) {
-                        edge.edgeToQuery(QueryType.ADD_EDGE)
+                        real_edge.edgeToQuery(QueryType.ADD_EDGE)
                     } else {
-                        edge.edgeToQuery(QueryType.REMOVE_EDGE)
+                        real_edge.edgeToQuery(QueryType.REMOVE_EDGE)
                     }
                 }
             } else {
